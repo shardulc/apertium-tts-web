9321b42ef4831de83291a61e713c20d7d779c214
Shardul Chiplunkar <shardul.chiplunkar@gmail.com>
2018-02-13 22:44:13 -0800

    Pass text too

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var apy = 'http://localhost:2737';
 var currentAudioUrl;
 var loadables = document.getElementsByClassName('loadable');
 var aboutModal = document.getElementById('aboutModalContainer');
@@ -5,8 +6,8 @@ var aboutModal = document.getElementById('aboutModalContainer');
 function loadAudio() {
     document.getElementById('loadingCircle').classList.remove('hidden');
     var xhr = new XMLHttpRequest();
-    var audio = document.getElementById('audioElement') || new Audio();
-    var url = 'http://localhost:2737/tts?lang=eng&q=hello';
+    var audio = document.getElementById('audioElement');
+    var url = apy + '/tts?lang=eng&q=' + document.getElementById('input').value;
 
     xhr.open('GET', encodeURI(url), true);
     xhr.setRequestHeader('Content-Type', 'application/json');
